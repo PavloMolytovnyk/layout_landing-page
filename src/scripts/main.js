a068c00d9a9e7d203fb562880d6d38896950476e
PavloMolytovnyk <pavlomolytovny@icloud.com>
2026-04-26 11:01:51 +0300

    Change 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.contacts-fields');
+
+    console.log(form);
+
+    if(form){
+        
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            form.reset();
+        });
+        
+    }
+});
